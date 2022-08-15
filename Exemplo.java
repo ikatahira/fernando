fc53d9f9bfc874750c5d67b1686f795a4fe07ffa
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-08-15 23:39:38 +0000

    gravando a idade

--- Exemplo.java
+++ Exemplo.java
@@ -3,5 +3,7 @@ public class Exemplo{
     public static void main (String[] args) {
 
         System.out.println("Fernando");
+        int idade=32;
+        System.out.println("tenho "+ idade);
     }
 }
